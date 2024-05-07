fbe15fae785224afc5fd187babd18152f4e84ed4
nayisntaround87 <naylasalwahayati@gmail.com>
2024-05-07 15:17:02 +0700

    member access operator

---
+++ pointersatu.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa
+{
+    public :
+    int nim;
+
+    void showNim()
+    {
+        cout << "No Induk = " << nim << endl;
+    }
+
+};
+
+int main ()
+{
+    mahasiswa mhs{1};// object mhs
+    mhs.showNim(); // member access operator
+}
