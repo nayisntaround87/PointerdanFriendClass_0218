cc4f1acddbd6b3f476b527ebdd11f0f0d0f258c8
nayisntaround87 <naylasalwahayati@gmail.com>
2024-05-07 15:18:14 +0700

    pointer reference refMhs

--- pointersatu.cpp
+++ pointersatu.cpp
@@ -17,4 +17,9 @@ int main ()
 {
     mahasiswa mhs{1};// object mhs
     mhs.showNim(); // member access operator
+
+    mahasiswa &refMhs = mhs; // pointer reference refMhs
+    refMhs.nim = 2; // member access operator
+
+    
 }
